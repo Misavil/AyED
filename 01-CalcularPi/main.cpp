3aa241b5d0a682ce1bc6a707d1699a654bd3d928
@lex <mvilcahalanoca@frba.utn.edu.ar>
2023-06-02 11:40:01 -0300

    Update main.cpp

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -3,16 +3,14 @@
 #include<math.h>
 using namespace std;
 main(){
-    double c=0;
-    int i=0;
+    double a,c=0;
+    for (int i=0; i < 999990; i++){
 
-    //while(a=0){}
-    for (double a=0; c =3,141592; i++){
         a=pow((-1),i)/(2*i+1);
         c=a+c;
-    }       
-        cout<<"usando la Serie de Leibniz da "<<setprecision(8)<<fixed<<c<<" equivalente a pi/4"<<endl;
-        cout<<"pi es igual a "<<c*4<<endl;
+    }
+
+    cout<<"pi es igual a "<<setprecision(6)<<fixed<<c*4<<endl;
 
     return 0;
 }
