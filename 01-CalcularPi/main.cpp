677c2ca5e4b9012a5c2f7fbf239a4ee50089b308
@lex <mvilcahalanoca@frba.utn.edu.ar>
2023-07-20 14:23:19 -0300

    Update main.cpp

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -3,30 +3,19 @@
 #include<math.h>
 using namespace std;
 double pif;
-
 double pic(){
     double temp,p=0,pi=3.14159265359;
-    int iteracion=1,verdad=0;
-    do{
-        double diferencia=fabs(p-pi);
-        if (diferencia<=0.00000040359){
-        verdad=1;
+    double diferencia =fabs((p*4)-pi);
+    if (diferencia>0.00000000000000000000000000059){
+        for (int i=0; i < diferencia*10000000; i++){
+            temp=pow((-1),i)/(2*i+1);
+            p+=temp;
         }
-        else{
-            iteracion+=111110;
-            p=0;
-            for (int i=0; i < iteracion; i++){
-                temp=pow((-1),i)/(2*i+1);
-                p+=temp;
-            }
-        p*=4;
-        }
-    } while (verdad<0.5);
-    
-pif=p;
-return pif;}
-
-main(){
-    cout<<"pi es igual a "<<setprecision(6)<<fixed<<pic()<<endl;
+    }
+    pif=p*4;
+return pif;
+}
+int main(){
+    cout<<"pi es igual a "<<setprecision(11)<<fixed<<pic()<<endl;
     return 0;
 }
